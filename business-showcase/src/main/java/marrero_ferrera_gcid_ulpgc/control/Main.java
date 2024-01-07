8a9bc607c15e88b36b83c7e53290268a53e519fe
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2024-01-07 00:13:23 +0000

    Version of Main

--- business-showcase/src/main/java/marrero_ferrera_gcid_ulpgc/control/Main.java
+++ business-showcase/src/main/java/marrero_ferrera_gcid_ulpgc/control/Main.java
@@ -1,6 +1,13 @@
 package marrero_ferrera_gcid_ulpgc.control;
 
+import marrero_ferrera_gcid_ulpgc.control.schemas.EnergyPrice;
+import marrero_ferrera_gcid_ulpgc.control.schemas.Weather;
 import marrero_ferrera_gcid_ulpgc.view.ViewFirst;
+
+import java.time.Instant;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,9 +15,47 @@ public class Main {
         // Misma operación pero en la clase "ViewFinal" para mostrar los resultados tras las operaciones.
     }
 
-    public static void updateUserValues(String url, String topicNameWeather, String topicNameEnergy, float powerChargeSolar,
-                                        float powerChargeWind, float batteryCapacity, boolean recommendedHalfBattery) {
+    public static void updateUserValues(float latitude, float longitude, String url, String topicNameWeather, String topicNameEnergy, float powerChargeSolar,
+                                        float powerChargeWind, float batteryCapacity, boolean recommendedHalfBattery) throws MyManagerException {
+        Fetcher weatherFetcher = new DataLakeFetcher(topicNameWeather, new Weather().getSs(), Instant.now());
+        Fetcher energyFetcher = new DataLakeFetcher(topicNameEnergy, new EnergyPrice().getSs(), Instant.now());
+        Subscriber weatherSubscriber = new TopicSubscriber(topicNameWeather, "WeatherConsumer");
+        Subscriber energySubscriber = new TopicSubscriber(topicNameEnergy, "EnergyConsumer");
 
-        // Hacer operaciones con las variables en otra clase "Controller"
+        CountDownLatch latch = new CountDownLatch(2);
+
+        // Crear ExecutorService para ejecutar hilos
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        // Ejecutar fetcher, operate y topicSubscriber en hilos separados
+        executorService.submit(() -> {
+            weatherFetcher.fetchFiles();
+            // Operaciones adicionales si es necesario
+            try {
+                weatherSubscriber.receiveMessage();
+            } catch (MyManagerException e) {
+                throw new RuntimeException(e);
+            }
+            latch.countDown();
+        });
+
+        executorService.submit(() -> {
+            energyFetcher.fetchFiles();
+            // Operaciones adicionales si es necesario
+            try {
+                energySubscriber.receiveMessage();
+            } catch (MyManagerException e) {
+                throw new RuntimeException(e);
+            }
+            latch.countDown();
+        });
+
+        try {
+            // Esperar a que ambos hilos terminen
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new MyManagerException("a", e);
+        }
+        // Ejecutar fetcher, operate y topicSubscriber
     }
 }
