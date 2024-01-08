e0fd8b89f699738478aeb81c4052272d81b3e2c6
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2024-01-08 12:04:35 +0000

    datalake-builder now builds from both topics

--- datalake-builder/src/main/java/marrero_ferrera_gcid_ulpgc/control/Main.java
+++ datalake-builder/src/main/java/marrero_ferrera_gcid_ulpgc/control/Main.java
@@ -1,14 +1,43 @@
 package marrero_ferrera_gcid_ulpgc.control;
 
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws MyReceiverException {
-        String topicName = (args.length > 0) ? args[0] : "prediction.Weather";
-        String url = (args.length > 1) ? args[1] : "tcp://localhost:61616";
-        String clientID = (args.length > 2) ? args[2] : "anonymous";
-        String basePath = (args.length > 3) ? args[3] : "datalake" + File.separator + "eventstore" + File.separator;
-        TopicSubscriber subscriber = new TopicSubscriber(url, topicName, clientID, basePath);
-        subscriber.receiveMessage();
+        String topicNameWeather = (args.length > 0) ? args[0] : "prediction.Weather";
+        String topicNameEnergy = (args.length > 1) ? args[1] : "energy.MarketPrices";
+        String url = (args.length > 2) ? args[2] : "tcp://localhost:61616";
+        String clientID = (args.length > 3) ? args[3] : "anonymous";
+        String basePath = (args.length > 4) ? args[4] : "datalake" + File.separator + "eventstore" + File.separator;
+        TopicSubscriber subscriberWeather = new TopicSubscriber(url, topicNameWeather, clientID, basePath);
+        TopicSubscriber subscriberEnergy = new TopicSubscriber(url, topicNameEnergy, clientID, basePath);
+
+        CountDownLatch latch = new CountDownLatch(2);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(() -> {
+            try {
+                subscriberWeather.receiveMessage();
+            } catch (MyReceiverException e) {
+                throw new RuntimeException(e);
+            }
+            latch.countDown();
+        });
+
+        executorService.submit(() -> {
+            try {
+                subscriberEnergy.receiveMessage();
+            } catch (MyReceiverException e) {
+                throw new RuntimeException(e);
+            }
+            latch.countDown();
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new MyReceiverException("Error waiting for threads", e);
+        }
     }
 }
